1c3bef9a62fecf2ead056b3d6a980262583769c3
rafaelgrecco <ra122453@uem.br>
2022-10-22 18:01:42 -0300

    Improvements and connection to the db

--- crawler_ml.py
+++ crawler_ml.py
@@ -1,13 +1,14 @@
 # import os 
 # import uuid 
 from time import sleep
-from requests_html import HTML, HTMLSession
+from requests_html import HTMLSession
 from loguru import logger
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-from urllib.error import HTTPError
+# from bs4 import BeautifulSoup
+# from urllib.request import urlopen
+# from urllib.error import HTTPError
 import re
 from time import sleep
+from client import Client
 
 
 class CrawlerML():
@@ -21,6 +22,14 @@ class CrawlerML():
     
     def _get_next_page(self, url):
         return self.session.get(url=url)
+
+    def _get_page_content(self, url):
+
+        for _ in range(10):
+            response = self.session.get(url=url)
+            self.logger.info(response.status_code)
+            if response.status_code == 200:
+                return response
     
     def _get_results(self):
         self.logger.info("Buscando resultados")
@@ -31,19 +40,20 @@ class CrawlerML():
                     break
             except:
                 sleep(3)
-        
-        response_content = HTML(session=response, html=response.content)
+
         count = 1
         while True:
             self.logger.info(f"Página {count}")
-            results = response_content.xpath("//a[contains(@class, 'ui-search-result__content ui-search-link')]")
+            results = response.html.xpath("//a[@class='ui-search-result__content ui-search-link']")
             for result in results:
-                yield result    
-            next_page_url = response_content.xpath("//a[contains(@class, 'andes-pagination__link shops__pagination-link ui-search-link')]", first=True)
+                page_content = result.attrs["href"]
+                page_content = self._get_page_content(page_content)
+                yield page_content
+            
+            next_page_url = response.html.xpath("//a[@class='andes-pagination__link shops__pagination-link ui-search-link']", first=True)
         
             if next_page_url is not None:
                 url = next_page_url.attrs["href"]
-
                 for _ in range(10):
                     try:
                         response = self._get_next_page(url)
@@ -51,91 +61,90 @@ class CrawlerML():
                             break
                     except:
                         sleep(3)
-                response_content = HTML(session=response, html=response.content)
                 sleep(3)
                 count += 1
             else:
                 break
-        self.logger.info("Captura realizada com sucesso")
-
+            
     def _get_title(self, data):
-        try:
-            title = data.find("h1", attrs={"class": 'ui-pdp-title'})
-            title = title.get_text()
-        except:
-            pass
-        return title
+        title = data.html.xpath("//h1[@class='ui-pdp-title']", first=True)
+        
+        if title is not None:
+            return title.text
+        else:
+            return None
+
 
     def _get_price(self, data):
-        try:
-            price = data.find("span", attrs={"class":"andes-money-amount__fraction"})
-            price = price.get_text()
-        except:
-            pass
-        return price
+        price = data.html.xpath("//span[@class='andes-money-amount__fraction']", first=True)
+
+        if price is not None:
+            return price.text
+        else:
+            return None
 
     def _get_local(self, data):
-        try:
-            local = data.find("div", attrs={"id":"location"})
-            local = local.find("p", attrs={"class":"ui-pdp-color--BLACK ui-pdp-size--SMALL ui-pdp-family--REGULAR ui-pdp-media__title"})
-            local = local.get_text()
-        except:
-            pass
-        return local
+        local = data.html.xpath("//p[@class='ui-pdp-color--BLACK ui-pdp-size--SMALL ui-pdp-family--REGULAR ui-pdp-media__title']", first=True)
+        
+        if local is not None:
+            return local.text
+        else:
+            return None
 
     def _get_description(self, data):
-        try:
-            description = data.find("p", attrs={"class":"ui-pdp-description__content"})
-            description = description.get_text()
+        description = data.html.xpath("//p[@class='ui-pdp-description__content']", first=True)
 
-            description = re.sub("<br>", "", description)
-        except:
-            pass
-        return description
+        if description is not None:
+            description = re.sub("<br>", "", description.text)
+            return description
+        else:
+            return None
 
     def _get_caracteristicas(self, data):
-        try:
-            caracteristicas = data.find_all("span", attrs={"class":"andes-table__column--value"})
+        caracteristicas = data.html.xpath("//span[@class='andes-table__column--value']")
 
-            return {
-                "Área Total": caracteristicas[0].get_text(),
-                "Área Útil": caracteristicas[1].get_text(),
-                "Quartos": caracteristicas[2].get_text(),
-                "Banheiros": caracteristicas[3].get_text(),
-                "Garagens": caracteristicas[4].get_text()
-            }
+        try: 
+            area_total = caracteristicas[0].text if caracteristicas[0].text is not None else None,
+            area_util = caracteristicas[1].text if caracteristicas[1].text is not None else None,
+            quartos = caracteristicas[2].text if caracteristicas[2].text is not None else None,
+            banheiros = caracteristicas[3].text if caracteristicas[3].text is not None else None
         except:
-            pass
+            caracteristicas = None
+
+        if caracteristicas is not None:
+            return {
+                "Área Total": area_total,
+                "Área Útil": area_util,
+                "Quartos": quartos,
+                "Banheiros": banheiros
+            }
+        else:
+            return None
 
     def _parse_data(self, data):
-        page = data.attrs["href"]
-        try:
-            html = urlopen(page)
-        except HTTPError as e:
-            logger.error(e)
-
-        try:
-            bs = BeautifulSoup(html.read(), "lxml")
-        except AttributeError as e:
-            logger.error(e)
-            return None
         
-        return {
-            "Título": self._get_title(bs),
-            "Preço": self._get_price(bs),
-            "Localização": self._get_local(bs),
-            "Descrição": self._get_description(bs),
-            "Características": self._get_caracteristicas(bs)
+        data = {
+            "Título": self._get_title(data),
+            "Preço": self._get_price(data),
+            "Localização": self._get_local(data),
+            "Descrição": self._get_description(data),
+            "Características": self._get_caracteristicas(data)
         }
 
+        if data is not None:
+            return data
+        else:
+            return None
+
     def get_data(self):
-        yield from (self._parse_data(result) for result in self._get_results())
+        yield from (self._parse_data(data) for data in self._get_results())
 
 if __name__ == "__main__":
     crawler = CrawlerML(
         session = HTMLSession(),
         logger = logger
     )
+    client = Client()
     logger.add("olx.log")
     for cont, data in enumerate(crawler.get_data()):
-        print(data)
+        client.upload_data(payload=data)
