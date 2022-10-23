7fe1f6ed9611056720035a65d14e1a13dd5e8578
rafaelgrecco <ra122453@uem.br>
2022-10-23 00:58:19 -0300

    fix to search number of pages

--- crawler_ml.py
+++ crawler_ml.py
@@ -1,15 +1,9 @@
-# import os 
-# import uuid 
 from time import sleep
 from requests_html import HTMLSession
 from loguru import logger
-# from bs4 import BeautifulSoup
-# from urllib.request import urlopen
-# from urllib.error import HTTPError
 import re
 from time import sleep
 from client import Client
-
 
 class CrawlerML():
     def __init__(self, session, logger) -> None:
@@ -24,7 +18,6 @@ class CrawlerML():
         return self.session.get(url=url)
 
     def _get_page_content(self, url):
-
         for _ in range(10):
             response = self.session.get(url=url)
             self.logger.info(response.status_code)
@@ -41,30 +34,45 @@ class CrawlerML():
             except:
                 sleep(3)
 
-        count = 1
-        while True:
-            self.logger.info(f"Página {count}")
+        pages = response.html.xpath("//li[@class='andes-pagination__page-count']", first=True)
+        pages = self._perform_regex(r'[0-9]+', pages.text)
+        pages = int(pages)
+        self.logger.info(f"Foram encontradas {pages} páginas")
+
+        for page_n in range(1, pages+1):
+            self.logger.info(f"Página {page_n} de {pages}")
             results = response.html.xpath("//a[@class='ui-search-result__content ui-search-link']")
             for result in results:
                 page_content = result.attrs["href"]
                 page_content = self._get_page_content(page_content)
                 yield page_content
-            
-            next_page_url = response.html.xpath("//a[@class='andes-pagination__link shops__pagination-link ui-search-link']", first=True)
-        
-            if next_page_url is not None:
-                url = next_page_url.attrs["href"]
-                for _ in range(10):
-                    try:
-                        response = self._get_next_page(url)
-                        if response.status_code == 200:
-                            break
-                    except:
+                
+            if page_n>1:
+                next_page_url = response.html.xpath("//a[@class='andes-pagination__link shops__pagination-link ui-search-link']", first=True)
+                
+                if next_page_url is not None:
+                    url = next_page_url.attrs["href"]
+                    for _ in range(10):
+                        try:
+                            response = self._get_next_page(url)
+                            if response.status_code == 200:
+                                break
+                        except:
+                            sleep(3)
                         sleep(3)
-                sleep(3)
-                count += 1
-            else:
-                break
+                else:
+                    break
+    
+    def _perform_regex(self, pattern, text):
+        self.logger.info("Performing " + pattern)
+        m = re.search(pattern, text)
+        try:
+            match = m.group(0)
+        except AttributeError:
+            self.logger.warning("Regex not found")
+            return None
+        self.logger.info("Found " + match)
+        return match
             
     def _get_title(self, data):
         title = data.html.xpath("//h1[@class='ui-pdp-title']", first=True)
@@ -73,7 +81,6 @@ class CrawlerML():
             return title.text
         else:
             return None
-
 
     def _get_price(self, data):
         price = data.html.xpath("//span[@class='andes-money-amount__fraction']", first=True)
@@ -104,20 +111,17 @@ class CrawlerML():
         caracteristicas = data.html.xpath("//span[@class='andes-table__column--value']")
 
         try: 
-            area_total = caracteristicas[0].text if caracteristicas[0].text is not None else None,
-            area_util = caracteristicas[1].text if caracteristicas[1].text is not None else None,
-            quartos = caracteristicas[2].text if caracteristicas[2].text is not None else None,
-            banheiros = caracteristicas[3].text if caracteristicas[3].text is not None else None
-        except:
-            caracteristicas = None
-
-        if caracteristicas is not None:
-            return {
-                "Área Total": area_total,
-                "Área Útil": area_util,
-                "Quartos": quartos,
-                "Banheiros": banheiros
+            data = {
+                "Área Total": caracteristicas[0].text if caracteristicas[0].text is not None else None,
+                "Área Útil": caracteristicas[1].text if caracteristicas[1].text is not None else None,
+                "Quartos": caracteristicas[2].text if caracteristicas[2].text is not None else None,
+                "Banheiros": caracteristicas[3].text if caracteristicas[3].text is not None else None
             }
+        except:
+            data = None
+
+        if data is not None:
+            return data
         else:
             return None
 
